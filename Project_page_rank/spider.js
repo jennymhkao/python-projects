3880d7938aaafcf134de0ffea5fd656d980fc3ab
jennymhkao <k.jennymh@gmail.com>
2020-05-06 16:27:57 +0800

    add example crawler

---
+++ Project_page_rank/spider.js
@@ -0,0 +1,15 @@
+spiderJson = {"nodes":[
+{"weight":2,"rank":2.590942282150764, "id":1, "url":"http://www.dr-chuck.com"},
+{"weight":2,"rank":19.0, "id":2, "url":"http://www.dr-chuck.com/dr-chuck/resume/index.htm"},
+{"weight":1,"rank":5.3545653692904605, "id":3, "url":"http://www.dr-chuck.com/office"},
+{"weight":1,"rank":0.0, "id":4, "url":"http://www.dr-chuck.com/sakai-book"},
+{"weight":1,"rank":2.590942282150764, "id":11, "url":"http://www.dr-chuck.com/dr-chuck/resume/leadership.htm"},
+{"weight":1,"rank":2.590942282150764, "id":13, "url":"http://www.dr-chuck.com/dr-chuck/resume/pictures/index.htm"}],
+"links":[
+{"source":0,"target":1,"value":3},
+{"source":0,"target":2,"value":3},
+{"source":0,"target":3,"value":3},
+{"source":1,"target":0,"value":3},
+{"source":1,"target":4,"value":3},
+{"source":1,"target":5,"value":3},
+{"source":4,"target":1,"value":3}]};
